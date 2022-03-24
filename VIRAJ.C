5290f1ae405eb1a238cafa74de8c1b9f0fc28ca2
virajsarvaiya <102276261+virajsarvaiya@users.noreply.github.com>
2022-03-24 11:36:13 +0530

    variable changed

--- VIRAJ.C
+++ VIRAJ.C
@@ -3,14 +3,14 @@
 #include<stdlib.h>
 void main()
 {
-	int *ptr;
+	int *ptr1;
 	int n,i;
 	clrscr();
 	printf("enter no. of element");
 	scanf("%d",&n);
-	ptr=(int*)malloc(n*sizeof(int));
+	ptr1=(int*)malloc(n*sizeof(int));
 
-	if(ptr==NULL)
+	if(ptr1==NULL)
 	{
 		printf("memory not allocated .\n");
 		exit(0);
@@ -20,17 +20,17 @@ void main()
 		printf("memory sucessfuly allocated using malloc .\n");
 		for(i=0; i<n; i++)
 		{
-			ptr[i]=i+1;
+			ptr1[i]=i+1;
 		}
 		printf("the element of the array are;");
 		for(i=0; i<n; i++)
 		{
-			ptr[i]=i+1;
+			ptr1[i]=i+1;
 		}
 		printf("the element of the array are:");
 		{
 			for(i=0; i<n; i++)
-			printf("%d",ptr[i]);
+			printf("%d",ptr1[i]);
 		}
 	}
 	getch();
