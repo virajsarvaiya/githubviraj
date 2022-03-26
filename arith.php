7a6fd0b0f083ad27e3ec8994707ad6694ae1e979
virajsarvaiya <102276261+virajsarvaiya@users.noreply.github.com>
2022-03-26 11:19:38 +0530

    arithmatic file
    addition ,subtraction ,multiplication ,division

---
+++ arith.php
@@ -0,0 +1,19 @@
+<html>
+<body>
+	<?php
+		$a=10;
+		$b=30;
+		$c=$a+$b;
+		echo "addition=$c";
+		echo "<br>";
+		$d=$a*$b;
+		echo "multiplication=$d";
+		echo "<br>";
+		$e=$a/$b;
+		echo "division=$e";
+		echo "<br>";
+		$f=$a-$b;
+		echo "substraction=$f";
+	?>
+</body>
+</html>	
