af6513a456ef99fc1390c3dba03a84eb8a050413
virajsarvaiya <102276261+virajsarvaiya@users.noreply.github.com>
2022-03-31 11:24:50 +0530

    index-array
    array of index

---
+++ indexarray.php
@@ -0,0 +1,21 @@
+<html>
+<body>
+	<?php
+		$a=array("admin","text","demo","hello","apple");
+		echo $a[0];
+		echo "<br>";
+		
+		echo $a[1];
+		echo "<br>";
+		
+		echo $a[2];
+		echo "<br>";
+		
+		echo $a[3];
+		echo "<br>";
+		
+		echo $a[4];
+	?>
+</body>
+</html>
+		
