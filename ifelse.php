4d947848828a85df60d2caa3e2a90c3c163acd34
virajsarvaiya <102276261+virajsarvaiya@users.noreply.github.com>
2022-03-31 11:24:08 +0530

    if else
    if-else

---
+++ ifelse.php
@@ -0,0 +1,15 @@
+<html>
+<body>
+	<?php
+		$a=50;
+		if($a>=90)
+			echo "disk";
+		else if ($a>=40)
+			echo "first";
+		else if ($a>=80)
+			echo "second";
+		else if ($a>=50)
+			echo "pass";
+	?>
+	</body>
+	</html>
