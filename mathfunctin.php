916d98b81ca460dc36ec22a7677234430868b2fd
virajsarvaiya <102276261+virajsarvaiya@users.noreply.github.com>
2022-04-06 11:08:13 +0530

    mathfunction
    all function of math

---
+++ mathfunctin.php
@@ -0,0 +1,28 @@
+<html>
+<head>
+<title>math function</title>
+</head>
+<body>
+	<?php
+		echo abs(-5);
+		echo "<br>";
+		echo ceil(2.4)."<br>";
+		echo floor(2.9)."<br>";
+		echo round(5.8)."<br>";
+		echo max(20,21)."<br>";
+		echo min(20,21)."<br>";
+		echo pow(2,3)."<br>";
+		echo sqrt(81)."<br>";
+		echo rand()."<br>";
+		echo cos(30)."<br>";
+		echo acos(0.52)."<br>";
+		echo bindec(10111)."<br>";
+		echo decbin(4)."<br>";
+		echo hexdec('a100')."<br>";
+		echo dechex(41216)."<br>";
+		echo log(100)."<br>";
+		echo is_finite(100)."<br>";
+		echo is_infinite(1)."<br>";
+	?>
+</body>
+</html>
